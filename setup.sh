2cabb0794339cae32cadbefd7cc7aa54804b9c0a
Aran <aran.borkum@mpb.com>
2024-09-25 15:42:26 +0100

    setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install bat
+brew install bat
+
+# install thefuck
+brew install thefuck
+
+# install eza
+brew install eza
+
+# install aerospace
+brew install --cask nikitabobko/tap/aerospace
