d8e68b5d8f7dedc6425f9a26b917e3973d2542ef
Aran Borkum <aran.borkum@kraken.tech>
2024-12-02 12:42:22 +0000

    Add LSP for bash

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed =  { "lua_ls", "ruff" }
+				ensure_installed =  { "lua_ls", "ruff", "bashls" }
 			})
 		end
 	},
@@ -19,6 +19,7 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.ruff.setup({})
+			lspconfig.bashls.setup({})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
